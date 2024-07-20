1e91686dccc97d824bcbdcd8310daa76dba38ff1
Jonathan Kulak <lackadaize@msn.com>
2024-07-20 11:26:01 -0400

    handleClick() switches 'ramen-detail' image, nameee, restaurant, rating, and comment based on image clicked in 'ramen-menu'

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 // index.js
 const ramensUrl = "http://localhost:3000/ramens/"
 
-
 // Callbacks
 const handleClick = (ramen) => {
-  let ramenSelected = ramen.id
-  console.log(ramenSelected)
+  let detailsImg = document.getElementById('detail-image')
+  let detailsName = document.getElementById('detail-name')
+  let detailsRestaurant = document.getElementById('detail-restaurant')
+  let detailsRating = document.getElementById('rating-display')
+  let detailsComment = document.getElementById('comment-display')
+  detailsImg.src = ramen.image
+  detailsName.textContent = ramen.name
+  detailsRestaurant.textContent = ramen.restaurant
+  detailsRating.textContent= ramen.rating
+  detailsComment.textContent = ramen.comment
 }
 
 const addSubmitListener = () => {
